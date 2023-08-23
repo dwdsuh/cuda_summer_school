c3d440f62de73f7bfd84e8c1da4f071cd3b28d74
daewon <dwdsuh@gmail.com>
2023-08-23 12:20:26 +0000

    including batch

--- model.cu
+++ model.cu
@@ -77,16 +77,17 @@ void initialize_model(const char *parameter_fname) {
   linear3_bias = new Tensor(buf, {2});
   buf += 2;
 
-  input = new Tensor({1, 256, 256});
-  output = new Tensor({2});
-  c1 = new Tensor({128, 254, 254});
-  i1 = new Tensor({128, 254, 254});
-  m1 = new Tensor({128, 127, 127});
-  c2 = new Tensor({256, 125, 125});
-  i2 = new Tensor({256, 125, 125});
-  m2 = new Tensor({256, 62, 62});
-  l1 = new Tensor({256, 62, 128});
-  l2 = new Tensor({256, 62, 64});
+  int batch = N;
+  input = new Tensor({batch, 1, 256, 256});
+  output = new Tensor({batch, 2});
+  c1 = new Tensor({batch, 128, 254, 254});
+  i1 = new Tensor({batch, 128, 254, 254});
+  m1 = new Tensor({batch, 128, 127, 127});
+  c2 = new Tensor({batch, 256, 125, 125});
+  i2 = new Tensor({batch, 256, 125, 125});
+  m2 = new Tensor({batch, 256, 62, 62});
+  l1 = new Tensor({batch, 256, 62, 128});
+  l2 = new Tensor({batch, 256, 62, 64});
 }
 // Conv2D
 // https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
@@ -128,52 +129,70 @@ static void linear(Tensor *in_t, Tensor *out_t, Tensor *weight_t,
 static void relu(Tensor *inout_t);
 
 void model_forward(float *inputN, float *outputN) {
-  for (int idx = 0; idx < N; idx++) {
+  int batch = N;
     //memcpy(input->buf, inputN + 256 * 256 * idx, 256 * 256 * sizeof(float));
+  fprintf(stderr, "%d\n", __LINE__);
     CHECK_CUDA(cudaMemcpy(
 		input->buf,
-		inputN + 256 * 256 * idx,
-		256 * 256 * sizeof(float),
+		inputN,
+		batch * 256 * 256 * sizeof(float),
 		cudaMemcpyHostToDevice
 	)
     );
+  fprintf(stderr, "%d\n", __LINE__);
 
     conv2d(input, c1, conv0_weight, conv0_bias);
+  fprintf(stderr, "%d\n", __LINE__);
     instancenorm2d(c1, i1, instanceNorm2d0_weight, instanceNorm2d0_bias);
+  fprintf(stderr, "%d\n", __LINE__);
     maxpool2d(i1, m1, 2, 2);
+  fprintf(stderr, "%d\n", __LINE__);
     relu(m1);
+  fprintf(stderr, "%d\n", __LINE__);
     conv2d(m1, c2, conv1_weight, conv1_bias);
+  fprintf(stderr, "%d\n", __LINE__);
     instancenorm2d(c2, i2, instanceNorm2d1_weight, instanceNorm2d1_bias);
+  fprintf(stderr, "%d\n", __LINE__);
     maxpool2d(i2, m2, 2, 2);
+  fprintf(stderr, "%d\n", __LINE__);
     relu(m2);
+  fprintf(stderr, "%d\n", __LINE__);
     linear(m2, l1, linear1_weight, linear1_bias);
+  fprintf(stderr, "%d\n", __LINE__);
     relu(l1);
+  fprintf(stderr, "%d\n", __LINE__);
     linear(l1, l2, linear2_weight, linear2_bias);
-    l2->reshape({1, 1015808});
+  fprintf(stderr, "%d\n", __LINE__);
+    l2->reshape({batch, 1, 1015808});
+  fprintf(stderr, "%d\n", __LINE__);
     linear(l2, output, linear3_weight, linear3_bias);
+  fprintf(stderr, "%d\n", __LINE__);
 
     //memcpy(outputN + 2 * idx, output->buf, 2 * sizeof(float));
     CHECK_CUDA(cudaMemcpy(
-		outputN + 2 * idx,
+		outputN,
 		output->buf,
-		2 * sizeof(float),
+		2 * sizeof(float) * batch,
 	        cudaMemcpyDeviceToHost
 		)
     );
-  }
+  fprintf(stderr, "%d\n", __LINE__);
 }
 
 __global__ void conv2d_kernel(float *in, float *out, float *weight, float *bias,
 	               int K, int C_IN, int C_OUT, int H_IN, int W_IN,
-		       int H_OUT, int W_OUT){
+		       int H_OUT, int W_OUT, int B){
   int tidx = blockDim.x * blockIdx.x + threadIdx.x;
   
-  int c_out = tidx / (H_OUT * W_OUT);
+  int b = tidx / (C_OUT * H_OUT * W_OUT);
+  int c_out = (tidx / (H_OUT * W_OUT)) % C_OUT;
   int h_out = (tidx / W_OUT) % H_OUT;
   int w_out = tidx % W_OUT;
 
-  if (c_out >= C_OUT || h_out >= H_OUT || w_out >= W_OUT) return;
+  if (b >= B || c_out >= C_OUT || h_out >= H_OUT || w_out >= W_OUT) return;
 
+  out += b * C_OUT * H_OUT * W_OUT;
+  in += b * C_IN * H_IN * W_IN;
   out[c_out * H_OUT * W_OUT + h_out * W_OUT + w_out] = bias[c_out];
   for (int c_in = 0; c_in < C_IN; c_in++) {
     for (int kh = 0; kh < K; kh++) {
@@ -198,29 +217,34 @@ static void conv2d(Tensor *in_t, Tensor *out_t, Tensor *weight_t,
   int C_IN = weight_t->shape[1];  //=in_t->shape[0];
   int C_OUT = weight_t->shape[0]; //=out_t->shape[0];
 
-  int H_IN = in_t->shape[1];
-  int W_IN = in_t->shape[2];
+  int batch = in_t->shape[0];
+  int H_IN = in_t->shape[2];
+  int W_IN = in_t->shape[3];
   int H_OUT = H_IN - K + 1; //=out_t->shape[1];
   int W_OUT = W_IN - K + 1; //=out_t->shape[2];
-  int n_thread = C_OUT * H_OUT * W_OUT;
+  int n_thread = batch * C_OUT * H_OUT * W_OUT;
   dim3 blockDim(512);
   dim3 gridDim((n_thread + 511) / 512);
 
-  conv2d_kernel<<<gridDim, blockDim>>>(in, out, weight, bias, K, C_IN, C_OUT, H_IN, W_IN, H_OUT, W_OUT);
+  conv2d_kernel<<<gridDim, blockDim>>>(in, out, weight, bias, K, C_IN, C_OUT, H_IN, W_IN, H_OUT, W_OUT, batch);
 
 }
 
 
 __global__ void instancenorm2d_kernel(float *in, float *out, float *weight, float *bias,
-	                              int C, int H, int W){
-  int c = blockDim.x * blockIdx.x + threadIdx.x;
-  if (c >= C) return;
+	                              int C, int H, int W, int B){
+  //int c = blockDim.x * blockIdx.x + threadIdx.x;
+  int tidx = blockDim.x * blockIdx.x + threadIdx.x;
+  int b = tidx / C;
+  int c = tidx % C;
+  
+  if (b >= B || c >= C) return;
   float e = 0, v = 0;
 
   // Caculate mean
   for (int h = 0; h < H; h++) {
     for (int w = 0; w < W; w++) {
-      e += in[c * H * W + h * W + w];
+      e += in[b * C * H * W + c * H * W + h * W + w];
     }
   }
   //calc_mean_kernel<<<1,1>>>(in, H, W, c, &e);
@@ -229,15 +253,15 @@ __global__ void instancenorm2d_kernel(float *in, float *out, float *weight, floa
   // Caculate Variance
   for (int h = 0; h < H; h++) {
     for (int w = 0; w < W; w++) {
-      v += (in[c * H * W + h * W + w] - e) * (in[c * H * W + h * W + w] - e);
+      v += (in[b * C * H * W + c * H * W + h * W + w] - e) * (in[b * C * H * W + c * H * W + h * W + w] - e);
     }
   }
   v /= H * W;
 
   for (int h = 0; h < H; h++) {
     for (int w = 0; w < W; w++) {
-      out[c * H * W + h * W + w] =
-          (in[c * H * W + h * W + w] - e) / sqrt(v + 1e-5) * weight[c] +
+      out[b * C * H * W + c * H * W + h * W + w] =
+          (in[b * C * H * W + c * H * W + h * W + w] - e) / sqrt(v + 1e-5) * weight[c] +
           bias[c];
     }
   }
@@ -252,28 +276,31 @@ static void instancenorm2d(Tensor *in_t, Tensor *out_t, Tensor *weight_t,
   float *weight = weight_t->buf;
   float *bias = bias_t->buf;
 
-  int C = in_t->shape[0]; //=out_t->shape[0];
-  int H = in_t->shape[1]; //=out_t->shape[1];
-  int W = in_t->shape[2]; //=out_t->shape[2];
+  int B = in_t->shape[0];
+  int C = in_t->shape[1]; //=out_t->shape[0];
+  int H = in_t->shape[2]; //=out_t->shape[1];
+  int W = in_t->shape[3]; //=out_t->shape[2];
+  int n_thread = C * B;
   dim3 blockDim(512);
-  dim3 gridDim((C + 512 -1) / 512);
+  dim3 gridDim((n_thread + 512 -1) / 512);
 
-  instancenorm2d_kernel<<<gridDim, blockDim>>>(in, out, weight, bias, C, H, W);
+  instancenorm2d_kernel<<<gridDim, blockDim>>>(in, out, weight, bias, C, H, W, B);
 }
 
 __global__ void linear_kernel(float *in, float *out, float *weight, float *bias,
-	                      int H_IN, int H_OUT, int N){
+	                      int H_IN, int H_OUT, int N, int batch){
   int tidx = blockDim.x * blockIdx.x + threadIdx.x;
 
-  int n = tidx / H_OUT;
+  int b = tidx / (N * H_OUT);
+  int n = (tidx / H_OUT) % N;
   int h_out = tidx % H_OUT;
 
-  if (n >= N || h_out >= H_OUT) return;
+  if (b >= batch || n >= N || h_out >= H_OUT) return;
 
-  out[n * H_OUT + h_out] = bias[h_out];
+  out[b * N * H_OUT + n * H_OUT + h_out] = bias[h_out];
   for (int h_in = 0; h_in < H_IN; h_in++) {
-    out[n * H_OUT + h_out] +=
-        in[n * H_IN + h_in] * weight[h_out * H_IN + h_in];
+    out[b * N * H_OUT + n * H_OUT + h_out] +=
+        in[b * N * H_IN + n * H_IN + h_in] * weight[h_out * H_IN + h_in];
   }
   /*
   for (int n = 0; n < N; n++) {
@@ -294,33 +321,36 @@ static void linear(Tensor *in_t, Tensor *out_t, Tensor *weight_t,
   float *weight = weight_t->buf;
   float *bias = bias_t->buf;
 
-  int H_IN = weight_t->shape[0];  // in_t의 마지막 차원
-  int H_OUT = weight_t->shape[1]; // out_t의 마지막 차원
+  int batch = in_t->shape[0];
+  int H_IN = weight_t->shape[2];  // in_t의 마지막 차원
+  int H_OUT = weight_t->shape[3]; // out_t의 마지막 차원
 
-  int N = in_t->get_elem() / H_IN; //=out_t->get_elem()/H_OUT
-  int n_thread = N * H_OUT;
+  int N = in_t->get_elem() / H_IN / batch ; //=out_t->get_elem()/H_OUT
+  // get_elem() already include batch
+  int n_thread = N * H_IN * batch;
   dim3 blockDim(512);
   dim3 gridDim((n_thread + 512 - 1) / 512);
-  linear_kernel<<<gridDim, blockDim>>>(in, out, weight, bias, H_IN, H_OUT, N);
+  linear_kernel<<<gridDim, blockDim>>>(in, out, weight, bias, H_IN, H_OUT, N, batch);
 }
 
 __global__ void maxpool2d_kernel(float *in, float *out, 
-	int H_IN, int W_IN, int H_OUT, int W_OUT, int N, int kH, int kW){
+	int H_IN, int W_IN, int H_OUT, int W_OUT, int N, int kH, int kW, int batch){
   int tidx = blockDim.x * blockIdx.x + threadIdx.x;
 
-  int n = tidx / (H_OUT * W_OUT);
+  int b = tidx / (N * H_OUT * W_OUT);
+  int n = (tidx / (H_OUT * W_OUT)) % N;
   int h_out = (tidx / W_OUT) % H_OUT;
   int w_out = tidx % W_OUT;
 
-  if (n >= N || h_out >= H_OUT || w_out >= W_OUT) return;
+  if (b >= batch || n >= N || h_out >= H_OUT || w_out >= W_OUT) return;
 
-  out[n * H_OUT * W_OUT + h_out * W_OUT + w_out] =
-      in[n * H_IN * W_IN + (h_out * kH) * H_IN + (w_out * kW)];
+  out[b * N * H_OUT * W_OUT + n * H_OUT * W_OUT + h_out * W_OUT + w_out] =
+      in[b * N * H_IN * W_IN + n * H_IN * W_IN + (h_out * kH) * H_IN + (w_out * kW)];
   for (int kh = 0; kh < kH; kh++)
     for (int kw = 0; kw < kW; kw++)
-      out[n * H_OUT * W_OUT + h_out * W_OUT + w_out] =
-          fmaxf(out[n * H_OUT * W_OUT + h_out * W_OUT + w_out],
-                in[n * H_IN * W_IN + (h_out * kH + kh) * H_IN +
+      out[b * N * H_OUT * H_IN + n * H_OUT * W_OUT + h_out * W_OUT + w_out] =
+          fmaxf(out[b * N * H_OUT * H_IN + n * H_OUT * W_OUT + h_out * W_OUT + w_out],
+                in[b * N * H_IN * W_IN + n * H_IN * W_IN + (h_out * kH + kh) * H_IN +
                    (w_out * kW + kw)]);
 }
 
@@ -328,16 +358,17 @@ static void maxpool2d(Tensor *in_t, Tensor *out_t, int kH, int kW) {
   float *in = in_t->buf;
   float *out = out_t->buf;
 
-  int H_IN = in_t->shape[1];
-  int W_IN = in_t->shape[2];
+  int H_IN = in_t->shape[2];
+  int W_IN = in_t->shape[3];
   int H_OUT = H_IN / kH; // =out_t->shape[1];
   int W_OUT = W_IN / kW; // =out_t->shape[2];
 
-  int N = in_t->shape[0];
-  int n_thread = N * H_OUT * W_OUT;
+  int batch = in_t->shape[0];
+  int N = in_t->shape[1];
+  int n_thread = batch * N * H_OUT * W_OUT;
   dim3 blockDim(512);
   dim3 gridDim((n_thread + 512 -1) / 512);
-  maxpool2d_kernel<<<gridDim, blockDim>>>(in, out, H_IN, W_IN, H_OUT, W_OUT, N, kH, kW);
+  maxpool2d_kernel<<<gridDim, blockDim>>>(in, out, H_IN, W_IN, H_OUT, W_OUT, N, kH, kW, batch);
 }
 
 __global__ void relu_kernel(float *inout, int N){
